04374e75b421493590884c7404cfeb83bdea9289
Nur Adnin Suhaila Sa'id <adninsuhaila@gmail.com>
2026-06-04 01:02:31 +0800

    Remove database files from project

--- Ticket_Cinema/Ticket_Cinema/Form1.cs
+++ Ticket_Cinema/Ticket_Cinema/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// Testing Commit
 namespace Ticket_Cinema
 {
     public partial class Form1 : Form
