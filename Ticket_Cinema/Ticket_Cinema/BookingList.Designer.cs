21ab19c6197efc0c2f8e70ff9564fbf7bc6ebc9e
adnin15 <adninsuhaila@gmail.com>
2026-06-10 05:37:16 +0800

    Added BookingList form design

---
+++ Ticket_Cinema/Ticket_Cinema/BookingList.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Ticket_Cinema
+{
+    partial class BookingList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvBookingList = new System.Windows.Forms.DataGridView();
+            this.BookingID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MovieName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Showtime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hall = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Seat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ticket = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookingList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBookingList
+            // 
+            this.dgvBookingList.AllowUserToResizeRows = false;
+            this.dgvBookingList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBookingList.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvBookingList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvBookingList.ColumnHeadersHeight = 35;
+            this.dgvBookingList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BookingID,
+            this.MovieName,
+            this.Showtime,
+            this.Hall,
+            this.Seat,
+            this.Amount,
+            this.Status,
+            this.Ticket});
+            this.dgvBookingList.Location = new System.Drawing.Point(12, 13);
+            this.dgvBookingList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvBookingList.Name = "dgvBookingList";
+            this.dgvBookingList.ReadOnly = true;
+            this.dgvBookingList.RowHeadersVisible = false;
+            this.dgvBookingList.RowHeadersWidth = 51;
+            this.dgvBookingList.RowTemplate.Height = 24;
+            this.dgvBookingList.Size = new System.Drawing.Size(952, 280);
+            this.dgvBookingList.TabIndex = 0;
+            // 
+            // BookingID
+            // 
+            this.BookingID.HeaderText = "Booking ID";
+            this.BookingID.MinimumWidth = 6;
+            this.BookingID.Name = "BookingID";
+            this.BookingID.ReadOnly = true;
+            // 
+            // MovieName
+            // 
+            this.MovieName.HeaderText = "Movie Name";
+            this.MovieName.MinimumWidth = 6;
+            this.MovieName.Name = "MovieName";
+            this.MovieName.ReadOnly = true;
+            // 
+            // Showtime
+            // 
+            this.Showtime.HeaderText = "Showtime";
+            this.Showtime.MinimumWidth = 6;
+            this.Showtime.Name = "Showtime";
+            this.Showtime.ReadOnly = true;
+            // 
+            // Hall
+            // 
+            this.Hall.HeaderText = "Hall";
+            this.Hall.MinimumWidth = 6;
+            this.Hall.Name = "Hall";
+            this.Hall.ReadOnly = true;
+            // 
+            // Seat
+            // 
+            this.Seat.HeaderText = "Seat";
+            this.Seat.MinimumWidth = 6;
+            this.Seat.Name = "Seat";
+            this.Seat.ReadOnly = true;
+            // 
+            // Amount
+            // 
+            this.Amount.HeaderText = "Total Amount";
+            this.Amount.MinimumWidth = 6;
+            this.Amount.Name = "Amount";
+            this.Amount.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // Ticket
+            // 
+            this.Ticket.HeaderText = "Ticket";
+            this.Ticket.MinimumWidth = 6;
+            this.Ticket.Name = "Ticket";
+            this.Ticket.ReadOnly = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(816, 469);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(148, 70);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // BookingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(992, 566);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvBookingList);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "BookingList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BookingList";
+            this.Load += new System.EventHandler(this.BookingList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookingList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBookingList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookingID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MovieName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Showtime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hall;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Seat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.DataGridViewLinkColumn Ticket;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Ticket_Cinema/Ticket_Cinema/BookingList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ticket_Cinema
+{
+    public partial class BookingList : Form
+    {
+        public BookingList()
+        {
+            InitializeComponent();
+        }
+
+        private void BookingList_Load(object sender, EventArgs e)
+        {
+            dgvBookingList.Rows.Add(
+                "BKG20260525001",
+                "Avengers Endgame",
+                "25 May 2026 08:00 PM",
+                "Hall 1",
+                "A2",
+                "RM 20.00",
+                "Completed",
+                "View"
+            );
+
+            dgvBookingList.Rows.Add(
+                "BKG20260520002",
+                "The Dark Knight",
+                "20 May 2026 04:00 PM",
+                "Hall 2",
+                "C5",
+                "RM 20.00",
+                "Completed",
+                "View"
+            );
+
+            foreach (DataGridViewRow row in dgvBookingList.Rows)
+            {
+                if (row.Cells["Status"].Value != null &&
+                    row.Cells["Status"].Value.ToString() == "Completed")
+                {
+                    row.Cells["Status"].Style.ForeColor = Color.Green;
+                    row.Cells["Status"].Style.Font =
+                        new Font("Segoe UI", 9, FontStyle.Bold);
+                }
+            }
+        }
+    }
+}
