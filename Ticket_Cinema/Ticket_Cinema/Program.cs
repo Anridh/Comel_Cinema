9ea92ca19ff55045d731e69e74bc4b2e3384b039
Anridh <11anwarridhwan@gmail.com>
2026-06-11 02:06:11 +0800

    update program

--- Ticket_Cinema/Ticket_Cinema/Program.cs
+++ Ticket_Cinema/Ticket_Cinema/Program.cs
@@ -16,7 +16,7 @@ namespace Ticket_Cinema
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PaymentGW());
+            Application.Run(new SignupF1());
         }
     }
 }
