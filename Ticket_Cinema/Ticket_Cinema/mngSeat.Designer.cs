c8c64f04fdf92291b7e7005d2f4f9ab869581367
Syuhada279 <d032410279@student.utem.edu.my>
2026-06-19 12:29:46 +0800

    adnin dan ayuni coding boleh mula

---
+++ Ticket_Cinema/Ticket_Cinema/mngSeat.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Ticket_Cinema
+{
+    partial class mngSeat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uptBtn = new Ticket_Cinema.RoundedButton();
+            this.ClearBtn = new Ticket_Cinema.RoundedButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BackBtn = new Ticket_Cinema.RoundedButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblHall = new Ticket_Cinema.RoundedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(789, 300);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 28);
+            this.textBox1.TabIndex = 58;
+            // 
+            // uptBtn
+            // 
+            this.uptBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(59)))), ((int)(((byte)(171)))));
+            this.uptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uptBtn.FlatAppearance.BorderSize = 0;
+            this.uptBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.uptBtn.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uptBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.uptBtn.Location = new System.Drawing.Point(1043, 292);
+            this.uptBtn.Margin = new System.Windows.Forms.Padding(50, 10, 60, 0);
+            this.uptBtn.Name = "uptBtn";
+            this.uptBtn.Size = new System.Drawing.Size(160, 49);
+            this.uptBtn.TabIndex = 57;
+            this.uptBtn.Text = "Update";
+            this.uptBtn.UseVisualStyleBackColor = false;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClearBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClearBtn.FlatAppearance.BorderSize = 0;
+            this.ClearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearBtn.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.ClearBtn.Location = new System.Drawing.Point(1043, 358);
+            this.ClearBtn.Margin = new System.Windows.Forms.Padding(50, 10, 60, 0);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(160, 49);
+            this.ClearBtn.TabIndex = 56;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(637, 358);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 30, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 24);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Status:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(637, 304);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 30, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Seat ID:";
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(33)))), ((int)(((byte)(62)))));
+            this.BackBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BackBtn.FlatAppearance.BorderSize = 0;
+            this.BackBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BackBtn.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.BackBtn.Location = new System.Drawing.Point(1032, 575);
+            this.BackBtn.Margin = new System.Windows.Forms.Padding(50, 50, 60, 30);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(160, 49);
+            this.BackBtn.TabIndex = 51;
+            this.BackBtn.Text = "Back";
+            this.BackBtn.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 231);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(50, 50, 3, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(555, 204);
+            this.dataGridView1.TabIndex = 50;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(33)))), ((int)(((byte)(62)))));
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1261, 119);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "MANAGE SEAT";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(789, 358);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 28);
+            this.textBox2.TabIndex = 59;
+            // 
+            // lblHall
+            // 
+            this.lblHall.BackColor = System.Drawing.Color.MidnightBlue;
+            this.lblHall.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblHall.FlatAppearance.BorderSize = 0;
+            this.lblHall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblHall.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHall.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.lblHall.Location = new System.Drawing.Point(503, 148);
+            this.lblHall.Margin = new System.Windows.Forms.Padding(50, 50, 60, 30);
+            this.lblHall.Name = "lblHall";
+            this.lblHall.Size = new System.Drawing.Size(273, 49);
+            this.lblHall.TabIndex = 60;
+            this.lblHall.Text = "Hall A";
+            this.lblHall.UseVisualStyleBackColor = false;
+            // 
+            // mngSeat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1261, 615);
+            this.Controls.Add(this.lblHall);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uptBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Name = "mngSeat";
+            this.Text = "mngSeat";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private RoundedButton uptBtn;
+        private RoundedButton ClearBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private RoundedButton BackBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private RoundedButton lblHall;
+    }
+}
